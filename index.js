d51504d5fa41deab7d909fe5fd74dc5d9477e95f
Zoheb Iqbal <zoheb.zafar.iqbal@gmail.com>
2022-04-18 15:51:58 -0400

    commit tree f8f3331a9c468a3bdf1f35ed621163d7c955246a

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main').remove();
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.innerHTML = 'Z is the champion'
